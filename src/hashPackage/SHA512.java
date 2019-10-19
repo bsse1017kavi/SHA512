eae0b51a02b427852b4e81952ef39e6f5a1ce2d7
bsse1017kavi <bsse1017@iit.du.ac.bd>
2019-10-19 19:10:34 +0600

    Generates hash

--- src/hashPackage/SHA512.java
+++ src/hashPackage/SHA512.java
@@ -157,34 +157,60 @@ public class SHA512
     {
         long t1,t2,a,b,c,d,e,f,g,h;
 
+        String [] keyInHex = {"428a2f98d728ae22", "7137449123ef65cd", "b5c0fbcfec4d3b2f", "e9b5dba58189dbbc",
+                "3956c25bf348b538", "59f111f1b605d019", "923f82a4af194f9b", "ab1c5ed5da6d8118", "d807aa98a3030242",
+                "12835b0145706fbe", "243185be4ee4b28c", "550c7dc3d5ffb4e2", "72be5d74f27b896f", "80deb1fe3b1696b1",
+                "9bdc06a725c71235", "c19bf174cf692694", "e49b69c19ef14ad2", "efbe4786384f25e3", "0fc19dc68b8cd5b5",
+                "240ca1cc77ac9c65", "2de92c6f592b0275", "4a7484aa6ea6e483", "5cb0a9dcbd41fbd4", "76f988da831153b5",
+                "983e5152ee66dfab", "a831c66d2db43210", "b00327c898fb213f", "bf597fc7beef0ee4", "c6e00bf33da88fc2",
+                "d5a79147930aa725", "06ca6351e003826f", "142929670a0e6e70", "27b70a8546d22ffc", "2e1b21385c26c926",
+                "4d2c6dfc5ac42aed", "53380d139d95b3df", "650a73548baf63de", "766a0abb3c77b2a8", "81c2c92e47edaee6",
+                "92722c851482353b", "a2bfe8a14cf10364", "a81a664bbc423001", "c24b8b70d0f89791", "c76c51a30654be30",
+                "d192e819d6ef5218", "d69906245565a910", "f40e35855771202a", "106aa07032bbd1b8", "19a4c116b8d2d0c8",
+                "1e376c085141ab53", "2748774cdf8eeb99", "34b0bcb5e19b48a8", "391c0cb3c5c95a63", "4ed8aa4ae3418acb",
+                "5b9cca4f7763e373", "682e6ff3d6b2b8a3", "748f82ee5defb2fc", "78a5636f43172f60", "84c87814a1f0ab72",
+                "8cc702081a6439ec", "90befffa23631e28", "a4506cebde82bde9", "bef9a3f7b2c67915", "c67178f2e372532b",
+                "ca273eceea26619c", "d186b8c721c0c207", "eada7dd6cde0eb1e", "f57d4f7fee6ed178", "06f067aa72176fba",
+                "0a637dc5a2c898a6", "113f9804bef90dae", "1b710b35131c471b", "28db77f523047d84", "32caab7b40c72493",
+                "3c9ebe0a15c9bebc", "431d67c49c100d4c", "4cc5d4becb3e42b6", "597f299cfc657e2a", "5fcb6fab3ad6faec",
+                "6c44198c4a475817"};
+
         long [] k = new long[80];
+
+
+        for(int i=0;i<80;i++)
+        {
+            k[i] = Long.parseUnsignedLong(keyInHex[i],16);
+        }
 
 
         long [] words = new long[80];
 
         for(int i=0;i<16;i++)
         {
-            words[i] = Long.parseUnsignedLong(block.substring(i,i*64));
+            words[i] = Long.parseUnsignedLong(block.substring(i*64,(i+1)*64-1),2);
         }
 
         for(int t=17;t<80;t++)
         {
-            words[t] = sigma1(words[t-2]) ^ words[t-7] ^ sigma0(words[t-15]) ^ words[t-16];
+            words[t] = modAdd(modAdd(sigma1(words[t-2]), words[t-7] ),modAdd( sigma0(words[t-15]) , words[t-16]));
         }
 
-        a = buffers[0];
-        b = buffers[1];
-        c = buffers[2];
-        d = buffers[3];
-        e = buffers[4];
-        f = buffers[5];
-        g = buffers[6];
-        h = buffers[7];
+
 
         for(int t=0;t<80;t++)
         {
-            t1 = modAdd(modAdd(modAdd(buffers[7],ch(buffers[4],buffers[5],buffers[6])),modAdd(Sigma1(buffers[4]),words[t])),k[t]);
-            t2 = modAdd(Sigma0(buffers[0]),maj(buffers[0],buffers[1],buffers[2]));
+            a = buffers[0];
+            b = buffers[1];
+            c = buffers[2];
+            d = buffers[3];
+            e = buffers[4];
+            f = buffers[5];
+            g = buffers[6];
+            h = buffers[7];
+
+            t1 = modAdd(modAdd(modAdd(h,ch(e,f,g)),modAdd(Sigma1(buffers[4]),words[t])),k[t]);
+            t2 = modAdd(Sigma0(a),maj(a,b,c));
 
             buffers[0] = modAdd(t1,t2);
             buffers[1] = a;
@@ -218,6 +244,10 @@ public class SHA512
             hash = moduloAdd(hash,f(blocks.get(i)));
         }
 
+        for(int i=0;i<hash.length;i++)
+        {
+            System.out.print(Long.toHexString(hash[i]));
+        }
 
     }
 
@@ -249,5 +279,6 @@ public class SHA512
         addPadding(this.binary);
         initializeBuffers();
         divide();
+        generateOutput();
     }
 }
--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -8,7 +8,7 @@ public class Main
     public static void main(String[] args)
     {
         SHA512 m = new SHA512();
-        //m.hash();
+        m.hash();
 
     }
 }
